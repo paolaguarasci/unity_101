d02f1b721c7066fa7cc5df98defb06c4883e6fe2
Paola Guarasci <paolaguarasci@protonmail.ch>
2020-04-01 19:03:22 +0200

    Rimozione bug movimento nemici

--- Assets/Scripts/WonderingAI.cs
+++ Assets/Scripts/WonderingAI.cs
@@ -28,6 +28,11 @@ public class WonderingAI : MonoBehaviour
 {
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
+    private bool _alive;
+
+    private void Start() {
+        _alive = true;
+    }
 
     private void Update()
     {
